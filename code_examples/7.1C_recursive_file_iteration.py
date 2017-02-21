2dd06f99537b92bf4f2deff83510926e5b790e6e
Thomas Nield <thomasnield@live.com>
2017-02-20 21:48:15 -0600

    more tweaks

--- code_examples/7.1C_recursive_file_iteration.py
+++ code_examples/7.1C_recursive_file_iteration.py
@@ -18,8 +18,5 @@ def recursive_files_in_directory(folder):
 
 recursive_files_in_directory('/home/thomas/Desktop/bbc_data_sets') \
     .filter(lambda f: f.endswith('.txt')) \
-    .flat_map(lambda f:  Observable.from_(open(f, encoding="ISO-8859-1"))) \
-    .map(lambda l: l.strip()) \
-    .filter(lambda l: l != "") \
     .subscribe(on_next=lambda l: print(l), on_error=lambda e: print(e))
 
